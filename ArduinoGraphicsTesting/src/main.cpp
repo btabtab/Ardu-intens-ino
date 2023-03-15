f60ad440b235620a0f7818bf244373c687a87fe2
btabtab <bladethace@gmail.com>
2023-03-15 06:55:03 +0000

    update?

--- ArduinoGraphicsTesting/src/main.cpp
+++ ArduinoGraphicsTesting/src/main.cpp
@@ -7,12 +7,13 @@
 #include <Adafruit_SSD1306.h>
 
 #include "JDevRendering/jdr_base_functions.h"
+#include "JDevRendering/jdr_super_functions.h"
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C screen(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void jdr_plot(int x, int y, Colour32 colour)
 {
-	screen.setDrawColor(!(!colour));
+	// screen.setDrawColor(!(!colour));
 	
 	/*
 	The modulo here will keep the pixel
@@ -39,6 +40,11 @@ void jdr_stopDrawing()
 	screen.sendBuffer();
 }
 
+void jdr_drawLine(int ax, int ay, int bx, int by, Colour32 colour)
+{
+	screen.drawLine(ax % 128, ay % 64, bx % 128, by % 64);
+}
+
 void setup()
 {
 	screen.begin();
@@ -47,10 +53,13 @@ void setup()
 
 void loop()
 {
-	static int z = 0;
+	static uint16_t z = 0, a = 0;
 	jdr_startDrawing();
 	{
-		jdr_plot((z >> 1) + 1, z++, 1);
+		jdr_plot(z - 1, a, 1);
+		jdr_plot(z, a, 1);
 	}
 	jdr_stopDrawing();
+	if(z==128){a++;}
+	z += 2;
 }
